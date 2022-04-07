b207f87604e8732acf52caef1bb524034ceb4996
M Anwari L <lm.anwari@gmail.com>
2022-04-07 22:07:00 +0700

    fixed bert initialization path handling.

--- utils/model.py
+++ utils/model.py
@@ -2,11 +2,12 @@ import json
 from transformers import BertForMaskedLM
 from models.mtl import MTModel
 from models.seqlab import DNABERTSeqLab
+from pathlib import Path
 
 # def init_mtl_model(pretrained_path: str, config: json, device="cpu"):
 def init_mtl_model(config_path: str):
     cfg = json.load(open(config_path, "r"))
-    bert = BertForMaskedLM.from_pretrained(cfg["pretrained"]).bert
+    bert = BertForMaskedLM.from_pretrained(Path(cfg["pretrained"])).bert
     model = MTModel(bert, cfg)
     return model
 
