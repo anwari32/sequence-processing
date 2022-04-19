d0ca612d85db31525a860b304e603dd9382787a1
M Anwari L <lm.anwari@gmail.com>
2022-04-19 09:48:54 +0700

    fixed path handling in tokenizer init.

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -1,5 +1,10 @@
+from pathlib import PureWindowsPath
+
+
 def get_default_tokenizer():
     from transformers import BertTokenizer
     from data_dir import pretrained_3kmer_dir
-    tokenizer = BertTokenizer.from_pretrained(pretrained_3kmer_dir)
+    from pathlib import Path, PureWindowsPath
+    pretrained_path = str(Path(PureWindowsPath(pretrained_3kmer_dir)))
+    tokenizer = BertTokenizer.from_pretrained(pretrained_path)
     return tokenizer
