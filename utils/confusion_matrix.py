3d0795a78a6667d12163cb7e73b9a59c4363444c
M Anwari L <lm.anwari@gmail.com>
2022-08-05 15:04:00 +0700

    added confusion matrix utils. added data analysis. added cf matrix for validation.

---
+++ utils/confusion_matrix.py
@@ -0,0 +1,50 @@
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import os
+import pandas as pd
+from tqdm import tqdm
+
+def filter_df_by_epoch(csv_file, epochs):
+    results = []
+    df = pd.read_csv(csv_file)
+    for e in epochs:
+        results.append(df[df["epoch"] == e])
+    return results
+
+def create_cf_matrix(df):
+    predictions = np.array([], int)
+    targets = np.array([], int)
+    for i, r in tqdm(df.iterrows(), total=df.shape[0], desc="Processing : "):
+        target = r["target"].split(' ')
+        target = [int(a) for a in target]
+        target = np.array(target, int)
+        targets = np.concatenate((targets, target))
+        prediction = r["prediction"].split(' ')
+        prediction = [int(a) for a in prediction]
+        prediction = np.array(prediction, int)
+        predictions = np.concatenate((predictions, prediction))
+
+    return predictions, targets
+
+def create_confusion_matrix(csv_file, epoch=0):
+    # raise NotImplementedError("Function is not implemented.")
+
+    df = pd.read_csv(csv_file)
+    df = df[df["epoch"] == epoch]
+    predictions = np.array([], int)
+    targets = np.array([], int)
+    for i, r in tqdm(df.iterrows(), total=df.shape[0], desc="Processing : "):
+        target = r["target"].split(' ')
+        target = [int(a) for a in target]
+        target = target[1:] # Discard CLS token label.
+        target = [a for a in target if a >= 0]
+        target = np.array(target, int)
+        targets = np.concatenate((targets, target))
+        prediction = r["prediction"].split(' ')
+        prediction = [int(a) for a in prediction]
+        prediction = prediction[1:] # Discard token label.
+        prediction = prediction[0:len(target)]
+        prediction = np.array(prediction, int)
+        predictions = np.concatenate((predictions, prediction))
+
+    return predictions, targets
