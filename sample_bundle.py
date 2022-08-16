d99d0f6c0e97522d391cfb0d65a1238790584999
M Anwari L <lm.anwari@gmail.com>
2022-08-16 10:31:56 +0700

    added sample script for testing purpose.

--- sample_bundle.py
+++ sample_bundle.py
@@ -11,18 +11,24 @@ if __name__ == "__main__":
 
     gene_train_bundle_10_path = os.path.join(dirpath, "gene_train_bundle.10.csv")
     gene_train_bundle_25_path = os.path.join(dirpath, "gene_train_bundle.25.csv")
+    gene_train_bundle_sample_path = os.path.join(dirpath, "gene_train_bundle.sample.csv")
     gene_validation_bundle_10_path = os.path.join(dirpath, "gene_validation_bundle.10.csv")
     gene_validation_bundle_25_path = os.path.join(dirpath, "gene_validation_bundle.25.csv")
+    gene_validation_bundle_sample_path = os.path.join(dirpath, "gene_validation_bundle.sample.csv")
     gene_test_bundle_10_path =os.path.join(dirpath, "gene_test_bundle.10.csv")
     gene_test_bundle_25_path =os.path.join(dirpath, "gene_test_bundle.25.csv")
+    gene_test_bundle_sample_path =os.path.join(dirpath, "gene_test_bundle.sample.csv")
 
-    for p, q, r in zip(
+    for p, q, r, s in zip(
         [gene_train_bundle_path, gene_validation_bundle_path, gene_test_bundle_path],
         [gene_train_bundle_10_path, gene_validation_bundle_10_path, gene_test_bundle_10_path],
-        [gene_train_bundle_25_path, gene_validation_bundle_25_path, gene_test_bundle_25_path]
+        [gene_train_bundle_25_path, gene_validation_bundle_25_path, gene_test_bundle_25_path],
+        [gene_train_bundle_sample_path, gene_validation_bundle_sample_path, gene_test_bundle_sample_path]
     ):
         source_df = pd.read_csv(p)
         p10_df = source_df.sample(frac=0.1, random_state=1337)
         p25_df = source_df.sample(frac=0.25, random_state=1337)
+        psample_df = source_df.sample(n=0.0001, random_state=1337)
         p10_df.to_csv(q, index=False)
         p25_df.to_csv(r, index=False)
+        psample_df.to_csv(s, index=False)
