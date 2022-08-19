87d8fea167f6cad35c73a5ef315c71a91a027ff3
M Anwari L <lm.anwari@gmail.com>
2022-08-19 07:57:36 +0700

    added split_log script

--- split_log.py
+++ split_log.py
@@ -26,10 +26,13 @@ if __name__ == "__main__":
     source_log = args.get("source-log", False)
     dest_dir = args.get("destination-dir", os.path.dirname(source_log))
 
+    print(f"Splitting {source_log}")
+    print(f"Store splitted log at {dest_dir}")
+
     df = pd.read_csv(source_log)
     epochs = df["epoch"].unique()
     fname = os.path.basename(source_log).split('.')[:-1]
     fname = '.'.join(fname)
-    for epoch in tqdm(epochs, total=len(epochs), desc="Processing log at epoch"):
+    for epoch in tqdm(epochs, total=len(epochs), desc="Processing log"):
         ndf = df[df["epoch"] == epoch]
-        ndf.to_csv(os.path.join(dest_dir, f"{dest_dir}.{epoch}.csv"), index=False)
+        ndf.to_csv(os.path.join(dest_dir, f"{fname}.{epoch}.csv"), index=False)
