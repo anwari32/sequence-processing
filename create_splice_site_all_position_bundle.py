a80bf36b905a740f7de4b17d66774601e23afd86
M Anwari L <lm.anwari@gmail.com>
2022-08-23 08:00:45 +0700

    fixed create ss code.

--- create_splice_site_all_position_bundle.py
+++ create_splice_site_all_position_bundle.py
@@ -14,7 +14,7 @@ def generate_splice_site_all_pos_bundle(gene_index, source_gene_dir, bundle_dest
     """
     os.makedirs(bundle_dest_dir, exist_ok=True)
     index_name = os.path.basename(gene_index)
-    index_name = index_name('.')[0]
+    index_name = index_name.split('.')[0]
     bundle_path = os.path.join(bundle_dest_dir, f"{index_name}_splice_site_all_pos.csv")
     intron_path = os.path.join(bundle_dest_dir, f"{index_name}_intron.csv")
     exon_path = os.path.join(bundle_dest_dir, f"{index_name}_exon.csv")
