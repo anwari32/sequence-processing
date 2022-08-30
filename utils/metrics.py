2f6d2af48d447cfb1e9970070742d81f521b33cc
M Anwari L <lm.anwari@gmail.com>
2022-08-30 11:59:36 +0700

    fixed Falses counting.

--- utils/metrics.py
+++ utils/metrics.py
@@ -69,7 +69,7 @@ class Metrics:
             elif pred_label == target_label:
                 self.Trues[pred_label] += 1
             else:
-                self.Falses[target_label] += 1
+                self.Falses[pred_label] += 1
 
     def get_precision(self, percentage=False):
         dummy = {}
