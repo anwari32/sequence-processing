a70ec822bc6747cb862f808e1ace5a865d053805
M Anwari L <lm.anwari@gmail.com>
2022-10-27 21:32:04 +0700

    training script.

--- training-balanced.sh
+++ training-balanced.sh
@@ -12,3 +12,6 @@ python run_sequence_labelling.py -t training/config/seqlab/ss-only.01.lr2e-5.jso
 python run_sequence_labelling.py -t training/config/seqlab/ss-only.01.lr3e-5.json -m models/config/seqlab/ -c freeze.base,base -d cuda:4 --device-list=4,5,6,7 --use-weighted-loss && 
 python run_sequence_labelling.py -t training/config/seqlab/ss-only.01.lr5e-5.json -m models/config/seqlab/ -c freeze.base,base -d cuda:4 --device-list=4,5,6,7 --use-weighted-loss
 
+
+python run_sequence_labelling.py -t training/config/seqlab/ss-only.01.lr5e-5.json -m models/config/seqlab/ -c base.lin1 -d cuda:4 --device-list=4,5,6,7 --use-weighted-loss
+
