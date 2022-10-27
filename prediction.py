01f65d46773ae0d77293094cb573f771d983936d
M Anwari L <lm.anwari@gmail.com>
2022-10-27 21:27:58 +0700

    begin prediction.

--- prediction.py
+++ prediction.py
@@ -38,36 +38,48 @@ def parse_args(argvs):
 
 
 if __name__ == "__main__":
-    args = parse_args(sys.argv[1:])
-    for key in args.keys():
-        print(f"# {key} - {args[key]}")
+    # args = parse_args(sys.argv[1:])
+    # for key in args.keys():
+    #     print(f"# {key} - {args[key]}")
+
+    # use static input first.
+    args = {
+        "device": "gpu:0",
+        "model-config": os.path.join("model", "config", "seqlab", "base.lin1.json"),
+        "model-checkpoint": os.path.join("run", "sso01-adamw-lr5e-5-base.lin1-2w1boplw", "latest", "checkpoint.pth"),
+        "test-config": os.path.join("training", "config", "seqlab", "ss-only.01.lr5e-5.json")
+    }
 
     device = args.get("device", "cpu") # specify device or use cpu otherwise.
 
     model_config_path = args.get("model-config", False)
     model_checkpoint = args.get("model-checkpoint", False)
-    test_config = args.get("test-config")
-    test_file = test_config.gt("test_data", False)
+    test_config_path = args.get("test-config")
+    test_config = json.load(open(test_config_path, "r"))
+    test_file = test_config.get("test_data", False)
 
     if not model_config_path:
         raise ValueError("model config not specified.")
     if not os.path.exists(model_config_path):
         raise ValueError(f"model config not exists at {model_config_path}")
-
+    print(f"using model config at {model_config_path}")
+        
     if not model_checkpoint:
         raise ValueError("model checkpoint not specified.")
     if not os.path.exists(model_checkpoint):
         raise ValueError(f"model checkpoint not exists at {model_checkpoint}")
+    print(f"found model checkpoint at {model_checkpoint}")
 
     if not test_file:
         raise ValueError("test not specified.")
     if not os.path.exists(test_file):
         raise ValueError(f"test file not exists at {test_file}")
+    print(f"found test data at {test_file}")
 
     bert_for_masked_lm = BertForMaskedLM.from_pretrained(os.path.join("pretrained", "3-new-12w-0"))
     model = DNABERT_SL(
         bert_for_masked_lm.bert, # bert, 
-        json.load(open(model_config_path)) # config
+        json.load(open(model_config_path, "r")) # config
     )
 
     checkpoint = torch.load(model_checkpoint, map_location=device)
@@ -86,7 +98,7 @@ if __name__ == "__main__":
         os.remove(logpath)
     os.makedirs(os.path.dirname(logpath), exist_ok=True)
     logfile = open(logpath, "x")
-    logfile.write("input_ids,prediction,target,accuracy\n")
+    logfile.write("step,input_ids,prediction,target\n")
 
     # initialize wandb.
     wandb.init(
@@ -104,19 +116,29 @@ if __name__ == "__main__":
             predictions, bert_output = model(input_ids, attn_mask)
             for inputs, pred, target_label in zip(input_ids, predictions, target_labels):
                 vals, pred_ids = torch.max(pred, 1)
+                
+                # log to local first.
+                input_ids_str = [str(a) for a in inputs]
+                input_ids_str = " ".join(input_ids_str)
+                pred_ids_str = [str(a) for a in pred_ids]
+                pred_ids_str = " ".join(pred_ids_str)
+                target_ids_str = [str(a) for a in target_label]
+                target_ids_str = " ".join(target_ids_str)
+
+                logfile.write(f"{prediction_step},{input_ids_str},{pred_ids_str},{target_ids_str}\n")
+
                 actual_input_ids = input_ids[1:] # remove CLS token
                 actual_input_ids = [t for t in actual_input_ids if t > 0]
                 actual_pred_ids = pred_ids[1:] # remove CLS prediction
                 actual_pred_ids = actual_pred_ids[0:len(actual_input_ids)]
-                target_ids = target_label[1:]
+                target_ids = target_label[1:] # remove CLS token
                 target_ids = target_label[0:len(actual_input_ids)]
 
                 metrics = Metrics(actual_pred_ids, target_ids)
                 for label_idx in range(NUM_LABELS):
                     wandb.log({
-                        f"precision-{Index_Dictionary[label_idx]}": metrics.precision(label_idx),
-                        f"recall-{Index_Dictionary[label_idx]}": metrics.recall(label_idx),
-                        
+                        f"prediction/precision-{Index_Dictionary[label_idx]}": metrics.precision(label_idx),
+                        f"prediction/recall-{Index_Dictionary[label_idx]}": metrics.recall(label_idx),
                         "prediction_step": prediction_step
                     })
 
