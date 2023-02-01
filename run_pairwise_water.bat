1bae47f7bab149aeec56040b0b21c617daff9124
M Anwari L <lm.anwari@gmail.com>
2023-02-01 12:29:20 +0700

    updated val data.

--- run_pairwise_water.bat
+++ run_pairwise_water.bat
@@ -1,4 +1,4 @@
-python run_pairwise_water.py -t "validation_f1_score_0.csv" -b error-analysis\data-comparison\validation_data.csv -d "water-validation_0.csv" -n 45
-python run_pairwise_water.py -t "validation_f1_score_01.csv" -b error-analysis\data-comparison\validation_data.csv -d "water-validation_01.csv" -n 45
-python run_pairwise_water.py -t "validation_f1_score_09.csv" -b error-analysis\data-comparison\validation_data.csv -d "water-validation_09.csv" -n 45
-python run_pairwise_water.py -t "validation_f1_score_1.csv" -b error-analysis\data-comparison\validation_data.csv -d "water-validation_1.csv" -n 45
+python run_pairwise_water.py -t "validation_f1_score_0.csv" -b error-analysis\data-comparison\validation_data.csv -d "water-validation_0.csv"
+python run_pairwise_water.py -t "validation_f1_score_01.csv" -b error-analysis\data-comparison\validation_data.csv -d "water-validation_01.csv"
+python run_pairwise_water.py -t "validation_f1_score_09.csv" -b error-analysis\data-comparison\validation_data.csv -d "water-validation_09.csv"
+python run_pairwise_water.py -t "validation_f1_score_1.csv" -b error-analysis\data-comparison\validation_data.csv -d "water-validation_1.csv"
