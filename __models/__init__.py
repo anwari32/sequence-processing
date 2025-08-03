5c9f08727e69bd9a7461c07e536288b2cb5ffb0f
M Anwari L <lm.anwari@gmail.com>
2025-08-03 17:18:38 +0700

    update gitignore

--- __models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .seqlab import *
-from ..dnabert.models.pretrained import *
-from .dnabert import *
-from .ensemble import *
--- static/run_binary_classification.py
+++
@@ -1,4 +0,0 @@
-from dnabert.models.binary_classification import DNABERT_BINARY
-import torch
-import numpy as np
-
